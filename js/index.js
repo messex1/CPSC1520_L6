3de98b7326dd99a37f36c6c6f568b514b61ce5d4
Michael Essex <112738703+messex1@users.noreply.github.com>
2024-04-27 15:41:55 -0600

    files added

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/*
+  HTML for the project.
+  Template
+  <div class="list-group-item list-group-item-action">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">FULLNAME HERE</h5>
+    </div>
+  <small>EMAIL HERE<</small>
+  </div>
+*/
